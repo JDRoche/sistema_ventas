0be5595744d55d2603805abdb481c7c775f7ec17
james0621 <james.cardona1996@gmail.com>
2021-09-07 11:42:26 -0500

    Se crea la estructura del proyecto en springboot

---
+++ backend/sistema_ventas/src/main/java/com/sistventas/SistemaVentasApplication.java
@@ -0,0 +1,13 @@
+package com.sistventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasApplication.class, args);
+	}
+
+}
---
+++ backend/sistema_ventas/src/test/java/com/sistventas/SistemaVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
